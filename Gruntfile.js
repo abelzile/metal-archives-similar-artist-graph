423cb5d8011ad4dd7059d3fb61e1b9d3799de76e
Andre Belzile <andre.belzile@gmail.com>
2014-12-11 22:36:22 -0700

    Updated Gruntfile for cleaner deployment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,13 @@ module.exports = function(grunt) {
 
                 files: [
 
-                    {expand: true, cwd: 'src/scripts/lib/', src: ['**'], dest: 'dist/scripts/lib'},
+                    {expand: true, cwd: 'src/scripts/lib/backbone', src: ['**/backbone.js'], dest: 'dist/scripts/lib/backbone'},
+                    {expand: true, cwd: 'src/scripts/lib/jquery/dist', src: ['**/jquery.min.js'], dest: 'dist/scripts/lib/jquery/dist'},
+                    {expand: true, cwd: 'src/scripts/lib/jquery-dialogextend', src: ['**/jquery.dialogextend.min.js'], dest: 'dist/scripts/lib/jquery-dialogextend'},
+                    {expand: true, cwd: 'src/scripts/lib/jqueryui-custom', src: ['**/*.min.js', '**/*.min.css', 'images/**'], dest: 'dist/scripts/lib/jqueryui-custom'},
+                    {expand: true, cwd: 'src/scripts/lib/lodash/dist', src: ['**/lodash.min.js'], dest: 'dist/scripts/lib/lodash/dist'},
+                    {expand: true, cwd: 'src/scripts/lib/requirejs', src: ['**/*.js'], dest: 'dist/scripts/lib/requirejs'},
+                    {expand: true, cwd: 'src/scripts/lib/requirejs-text', src: ['**/*.js'], dest: 'dist/scripts/lib/requirejs-text'},
                     {expand: true, cwd: 'src/images/', src: ['**'], dest: 'dist/images'},
                     {expand: true, cwd: 'src/templates/', src: ['**'], dest: 'dist/templates'},
                     {src: 'src/index.html', dest: 'dist/index.html'}
